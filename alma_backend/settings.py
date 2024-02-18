ca48efbe2f438807e61116882b075850d33ec0b9
esalvaggio <elliotms98@gmail.com>
2024-02-17 22:04:48 -0800

    CORS fixes

--- alma_backend/settings.py
+++ alma_backend/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
 
@@ -70,6 +71,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    env('LOCALHOST'),
 ]
 
 ROOT_URLCONF = 'alma_backend.urls'
