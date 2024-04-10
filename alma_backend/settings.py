0daa826972a1c1a4dfb2eae4f53708f0c74491bd
Elliot Salvaggio <elliotms98@gmail.com>
2024-04-10 00:14:08 -0700

    Adding allowed hosts

--- alma_backend/settings.py
+++ alma_backend/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') or ['localhost', '127.0.0.1']
+
 
 AUTH_USER_MODEL= "user.User"
 
