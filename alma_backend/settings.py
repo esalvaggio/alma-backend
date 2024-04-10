b88855fcb38e42ca9ac120cefeadcc17a342964c
Elliot Salvaggio <elliotms98@gmail.com>
2024-04-10 00:16:52 -0700

    Trying different host solution

--- alma_backend/settings.py
+++ alma_backend/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') or ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['alma-backend-e8edf244cc30.herokuapp.com', 'localhost', '127.0.0.1']
 
 AUTH_USER_MODEL= "user.User"
 
