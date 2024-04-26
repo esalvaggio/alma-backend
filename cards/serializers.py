4ed36b9c74b1d41b340734c2326974574bba73b2
Elliot Salvaggio <elliotms98@gmail.com>
2024-04-25 23:56:50 -0700

    Adding field to serializer

--- cards/serializers.py
+++ cards/serializers.py
@@ -4,4 +4,4 @@ from .models import Card
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
-        fields = ['id', 'essay', 'question', 'answer', 'next_review_date', 'review_interval', 'review_count']
+        fields = ['id', 'essay', 'question', 'answer', 'next_review_date', 'review_interval', 'review_count', 'percent_through']
