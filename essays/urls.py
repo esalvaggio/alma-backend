97d39b1fd5232c38a4e99c2c55ba608fd48605fd
Elliot Salvaggio <elliotms98@gmail.com>
2024-08-08 22:41:17 -0700

    Adding delete essays route

--- essays/urls.py
+++ essays/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EssayListCreateAPIView, EssayDetailAPIView, EssayMinimalListView
+from .views import EssayListCreateAPIView, EssayDetailAPIView, EssayMinimalListView, EssayListDeleteAPIView
 
 urlpatterns = [
     path('', EssayListCreateAPIView.as_view(), name='essay-list-create'),
+    path('<int:pk>/', EssayListDeleteAPIView.as_view(), name='essay-list-delete'),
     path('<int:pk>/', EssayDetailAPIView.as_view(), name='essay-detail'),
     path('minimal/', EssayMinimalListView.as_view(), name='essay-minimal-list'),
 ]
